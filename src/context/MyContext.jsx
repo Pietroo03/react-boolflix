2858a9c1f65a9930e5bdb936e3c97776355c7657
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-28 11:57:11 +0100

    add global state

---
+++ src/context/MyContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useState } from "react";
+
+export const MyContext = createContext()
+
+const API_KEY = 'de0058b6c855cc9f72269ef97bdc720a'
+const URL_movie = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`
+
+export const MyContextData = ({ children }) => {
+
+    const [movies, setMovies] = useState([])
+
+    const fetchMovies = (searchQuery) => {
+        fetch(`${URL_movie}${searchQuery}`)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+                setMovies(data.results)
+            })
+            .catch(error => {
+                console.error('Errore nella richiesta', error);
+            })
+    }
+
+    return (
+        <MyContext.Provider value={{ movies, fetchMovies }}>
+            {children}
+        </MyContext.Provider>
+    )
+}
