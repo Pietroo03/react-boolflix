b2372966aa523dac49e3be5f77c0b6bc4b81ae95
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-29 10:15:17 +0100

    add css to header

--- src/App.jsx
+++ src/App.jsx
@@ -46,54 +46,68 @@ function App() {
 
   return (
     <>
-      <div className='input-search'>
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={handleInput}
-          placeholder='Cerca un film...'
-        />
 
-        <button type='submit' onClick={handleSubmit}>Cerca</button>
-      </div>
-
-      <div className='list-movies'>
-        {movies.map((movie) => (
-
-          <div className="movie" key={movie.id}>
-            <h1>{movie.title}</h1>
-            <div>
-              <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="" />
-            </div>
-            <h4>{movie.original_title}</h4>
-            <div>
-              <Flag code={languageFlag[movie.original_language] || null} style={{ width: 30, height: 30 }} />
-            </div>
-
-            <span className='ms-2'>{Math.round(movie.vote_average / 2)}</span>
-            <span> {renderstars(movie.vote_average)}</span>
+      <header>
+        <div className='input-search m-4 d-flex align-items-center justify-content-between'>
+          <div>
+            <h1>LOGO</h1>
           </div>
-        ))}
-      </div>
-
-      <div className='list-tvSeries'>
-        {tvSeries.map((tvSerie) => (
-          <div className="tv-serie" key={tvSerie.id}>
-            <h1>{tvSerie.name}</h1>
-            <div>
-
-              <img src={`https://image.tmdb.org/t/p/w342${tvSerie.poster_path}`} alt="" />
+          <div className='d-flex'>
+            <input
+              className='form-control w-auto'
+              type="text"
+              value={searchQuery}
+              onChange={handleInput}
+              placeholder='Cerca un film...'
+            />
+            <div className='ps-4'>
+              <button type='submit' onClick={handleSubmit} className='btn btn-secondary ms-2'>Cerca</button>
             </div>
-            <h4>{tvSerie.original_name}</h4>
-            <div>
-              <Flag code={languageFlag[tvSerie.original_language] || null} style={{ width: 30, height: 30 }} />
-            </div>
-
-            <span className='ms-2'>{Math.round(tvSerie.vote_average / 2)}</span>
-            <span> {renderstars(tvSerie.vote_average)}</span>
           </div>
-        ))}
-      </div>
+        </div>
+
+      </header>
+
+      <main>
+        <div className='list-movies'>
+          {movies.map((movie) => (
+
+            <div className="movie" key={movie.id}>
+              <h1>{movie.title}</h1>
+              <div>
+                <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="" />
+              </div>
+              <h4>{movie.original_title}</h4>
+              <div>
+                <Flag code={languageFlag[movie.original_language] || null} style={{ width: 30, height: 30 }} />
+              </div>
+
+              <span className='ms-2'>{Math.round(movie.vote_average / 2)}</span>
+              <span> {renderstars(movie.vote_average)}</span>
+            </div>
+          ))}
+        </div>
+
+        <div className='list-tvSeries'>
+          {tvSeries.map((tvSerie) => (
+            <div className="tv-serie" key={tvSerie.id}>
+              <h1>{tvSerie.name}</h1>
+              <div>
+
+                <img src={`https://image.tmdb.org/t/p/w342${tvSerie.poster_path}`} alt="" />
+              </div>
+              <h4>{tvSerie.original_name}</h4>
+              <div>
+                <Flag code={languageFlag[tvSerie.original_language] || null} style={{ width: 30, height: 30 }} />
+              </div>
+
+              <span className='ms-2'>{Math.round(tvSerie.vote_average / 2)}</span>
+              <span> {renderstars(tvSerie.vote_average)}</span>
+            </div>
+          ))}
+        </div>
+
+      </main>
 
     </>
   )
