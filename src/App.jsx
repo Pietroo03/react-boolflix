63d5d4ae294938e698a6303c357ba2a305630504
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-29 12:06:38 +0100

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 function App() {
 
   const [searchQuery, setSearchQuery] = useState('')
+  const [showTitle, setShowTitles] = useState(false)
   const { movies, fetchMovies, tvSeries, fetchTvSeries } = useContext(MyContext)
 
   function handleInput(e) {
@@ -17,6 +18,7 @@ function App() {
     if (searchQuery !== '') {
       fetchMovies(searchQuery)
       fetchTvSeries(searchQuery)
+      setShowTitles(true)
     }
   }
 
@@ -70,72 +72,75 @@ function App() {
 
       <main>
         <div className="container">
-          <div className="mb-5">
-            <h1>Film</h1>
-            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-              {movies.map((movie) => (
+          {showTitle && (
+            <>
+              <div className="mb-5">
+                <h1 className='text-white'>Film</h1>
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                  {movies.map((movie) => (
 
-                <div className="movie col" key={movie.id}>
-                  <div className="card movie-card h-100">
-                    <div className='movie-img-container'>
-                      <img
-                        src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
-                        alt=""
-                        className='card-img-top movie-img'
-                        style={{ height: '500px', objectFit: 'cover' }}
-                      />
-                    </div>
-                    <div className="card-body movie-info">
-                      <h3 className='card-title'>{movie.title}</h3>
-                      <h6 className='card-subtitle mb-2'>{movie.original_title}</h6>
-                      <div style={{ overflowX: 'hidden' }}>{movie.overview}</div>
-                      <div>
-                        <Flag code={languageFlag[movie.original_language] || null} style={{ width: 30, height: 30 }} />
+                    <div className="movie col" key={movie.id}>
+                      <div className="card movie-card h-100">
+                        <div className='movie-img-container'>
+                          <img
+                            src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+                            alt=""
+                            className='card-img-top movie-img'
+                            style={{ height: '500px', objectFit: 'cover' }}
+                          />
+                        </div>
+                        <div className="card-body movie-info">
+                          <h3 className='card-title'>{movie.title}</h3>
+                          <h6 className='card-subtitle mb-2'>{movie.original_title}</h6>
+                          <div style={{ overflowX: 'hidden' }}>{movie.overview}</div>
+                          <div>
+                            <Flag code={languageFlag[movie.original_language] || null} style={{ width: 30, height: 30 }} />
+                          </div>
+                          <div className="d-flex align-items-center">
+                            <span className='ms-2'>{Math.round(movie.vote_average / 2)}</span>
+                            <span className='ps-2'> {renderstars(movie.vote_average)}</span>
+                          </div>
+                        </div>
                       </div>
-                      <div className="d-flex align-items-center">
-                        <span className='ms-2'>{Math.round(movie.vote_average / 2)}</span>
-                        <span className='ps-2'> {renderstars(movie.vote_average)}</span>
-                      </div>
                     </div>
-                  </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-          </div>
+              </div>
 
-          <div className="mb-5">
-            <h1>Serie Tv</h1>
-            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-              {tvSeries.map((tvSerie) => (
+              <div className="mb-5">
+                <h1 className='text-white'>Serie Tv</h1>
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                  {tvSeries.map((tvSerie) => (
 
-                <div className="tvSerie col" key={tvSerie.id}>
-                  <div className="card tvSerie-card h-100">
-                    <div className='tvSerie-img-container'>
-                      <img
-                        src={`https://image.tmdb.org/t/p/w342${tvSerie.poster_path}`}
-                        alt=""
-                        className='card-img-top tvSerie-img'
-                        style={{ height: '500px', objectFit: 'cover' }}
-                      />
-                    </div>
-                    <div className="card-body tvSerie-info">
-                      <h3 className='card-title'>{tvSerie.title}</h3>
-                      <h6 className='card-subtitle mb-2'>{tvSerie.original_title}</h6>
-                      <div>{tvSerie.overview}</div>
-                      <div>
-                        <Flag code={languageFlag[tvSerie.original_language] || null} style={{ width: 30, height: 30 }} />
+                    <div className="tvSerie col" key={tvSerie.id}>
+                      <div className="card tvSerie-card h-100">
+                        <div className='tvSerie-img-container'>
+                          <img
+                            src={`https://image.tmdb.org/t/p/w342${tvSerie.poster_path}`}
+                            alt=""
+                            className='card-img-top tvSerie-img'
+                            style={{ height: '500px', objectFit: 'cover' }}
+                          />
+                        </div>
+                        <div className="card-body tvSerie-info">
+                          <h3 className='card-title'>{tvSerie.title}</h3>
+                          <h6 className='card-subtitle mb-2'>{tvSerie.original_title}</h6>
+                          <div>{tvSerie.overview}</div>
+                          <div>
+                            <Flag code={languageFlag[tvSerie.original_language] || null} style={{ width: 30, height: 30 }} />
+                          </div>
+                          <div className="d-flex align-items-center">
+                            <span className='ms-2'>{Math.round(tvSerie.vote_average / 2)}</span>
+                            <span className='ps-2'> {renderstars(tvSerie.vote_average)}</span>
+                          </div>
+                        </div>
                       </div>
-                      <div className="d-flex align-items-center">
-                        <span className='ms-2'>{Math.round(tvSerie.vote_average / 2)}</span>
-                        <span className='ps-2'> {renderstars(tvSerie.vote_average)}</span>
-                      </div>
                     </div>
-                  </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-          </div>
-
+              </div>
+            </>
+          )}
         </div>
       </main>
 
